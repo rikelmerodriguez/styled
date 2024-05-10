09ba5b1be17547ff8e88ab883a09f4e118a7f7bf
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-05-10 12:48:27 -0300

    refactor: component vaga

--- src/components/Vaga/index.tsx
+++ src/components/Vaga/index.tsx
@@ -1,4 +1,4 @@
-import styles from './Vaga.module.css'
+import { Vagas, VagaTitulo, LinkdaMinhaVaga } from './styles'
 
 type Props = {
   titulo: string
@@ -11,8 +11,8 @@ type Props = {
 }
 
 const Vaga = (props: Props) => (
-  <li className={styles.vaga}>
-    <h3 className={styles.vagaTitulo}>{props.titulo}</h3>
+  <Vagas>
+    <VagaTitulo>{props.titulo}</VagaTitulo>
     <ul>
       <li>Localizacao: {props.localizacao}</li>
       <li>Senioridade: {props.nivel}</li>
@@ -22,10 +22,9 @@ const Vaga = (props: Props) => (
       </li>
       <li>Requisitos: {props.requisitos.join(', ')}</li>
     </ul>
-    <a className={styles.vagaLink} href="#">
+    <LinkdaMinhaVaga as="a" href="#">
       Ver detalhes e candidatar-se
-    </a>
-  </li>
+    </LinkdaMinhaVaga>
+  </Vagas>
 )
-
 export default Vaga
